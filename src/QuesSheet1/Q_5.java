20d46f7682c738c22de5880df8cac014f5ff03c9
twinkle-thakur <twinklethakur7879@gmail.com>
2025-03-13 11:56:12 +0530

    rorate an array by k steps

--- src/QuesSheet1/Q_5.java
+++ src/QuesSheet1/Q_5.java
@@ -1,41 +1,36 @@
 package QuesSheet1;
 
 public class Q_5 {
+public static void reverse(int[] a,int start,int end) {
+//	 start=a[0];
+//	 end=a.length;
+	 while(start<end) {
+		 int temp=a[start];
+		 a[start]=a[end];
+		 a[end]=temp;
+		 start++;
+		 end--;
+	 }
+}
+public static void rotateRight(int[] a,int d) {
+	d=d%a.length;
+	reverse(a, 0, a.length-1);
+	reverse(a, 0, d-1);
+	reverse(a, d, a.length-1);
 
-	public static void reverse(int[] arr,int start,int end) {
-		while(start<end) {
-			int temp=arr[start];
-			arr[start]=arr[end];
-			arr[end]=temp;
-			start++; end--;
-		}
-	}
-	
-	public static void rotate(int[] arr,int d) {
-		d=d%arr.length;
-		reverse(arr, 0, arr.length-1);
-		reverse(arr, 0,d-1);
-		reverse(arr, d, arr.length-1);
-	}
-	public static void main(String[] args) {
-	//	int[] a= {1,2,3,4,5};
-		int[] a= {2,4,6,8,10,12,14,16,18,20};
-		int d=3;//step size 
-		rotate(a, d);
-		for(int i:a) {
-			System.out.print(i+" ");
-		}
-		System.out.println("....");
-		for(int i=0;i<d;i++) {
-			int temp=a[0];
-			for(int j=0;j<a.length-1;j++) {
-				a[j]=a[j+1];
-			}
-			a[a.length-1]=temp;
-		}
-		for(int i=0;i<a.length;i++) {
-			System.out.print(a[i]+" ");
-		}
-	}
+}
+public static void rotateLeft(int[] a,int d) {
+	d=d%a.length;
+	reverse(a, 0, d-1);
+	reverse(a, d, a.length-1);
+	reverse(a, 0, a.length-1);
+}
 
+public static void main(String[] args) {
+	int[] a= {1,2,3,4,5};
+	rotateLeft(a, 2);
+	for(int i:a) {
+		System.out.print(i+" ");
+	}
+}
 }
