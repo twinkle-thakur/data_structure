16e7c1a49e35c8793d082478fe6132eb7b49fa90
twinkle-thakur <twinklethakur7879@gmail.com>
2025-03-16 22:58:21 +0530

    Frequencies of Limited Range Array Elements

---
+++ src/QuesSheet1/Q_9_2.java
@@ -0,0 +1,36 @@
+package QuesSheet1;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class Q_9_2 {
+	public static void frequency(int[] arr,int n,int p) {
+		int i=0,j;
+		while(i<n) {
+			if(arr[i]>0&&arr[i]<n) {
+				j=arr[i]-1;
+				if(arr[j]<=0) {
+					arr[i]=0;
+					arr[j]--;
+					i++;
+				}	else {
+					arr[i]=arr[j];
+					arr[j]=-1;
+				}
+			}else if(arr[i]>n) {
+				arr[i]=0;
+				i++;
+			}else {
+				i++;
+			}
+		}
+		for( i=0;i<n;i++) {
+			arr[i]=Math.abs(arr[i]);
+			System.out.print(arr[i]+" ");
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr= {3,4,4,8,1,2,5};
+		frequency(arr,7 , 8);
+	}
+}
