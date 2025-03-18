c066d8243e6d00bafcd0a1717558768eaf47af42
twinkle-thakur <twinklethakur7879@gmail.com>
2025-03-18 15:05:49 +0530

    removing duplicates from unsorted array using stream API

---
+++ src/QuestionSheet2/Q_5.java
@@ -0,0 +1,17 @@
+package QuestionSheet2;
+
+import java.util.Arrays;
+
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+public class Q_5 {
+	
+public static void main(String[] args) {
+	int[] a= {1,2,3};
+	List<Integer> list=Arrays.stream(a).boxed().distinct().collect(Collectors.toList());
+	
+	System.out.println(list);
+}
+}
