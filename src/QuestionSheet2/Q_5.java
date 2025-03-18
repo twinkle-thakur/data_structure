b4f10a7d177dbae77bc8d6587bc7b1327cd458e5
twinkle-thakur <twinklethakur7879@gmail.com>
2025-03-18 15:07:06 +0530

    duplicates removing in array using stream API

--- src/QuestionSheet2/Q_5.java
+++ src/QuestionSheet2/Q_5.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class Q_5 {
 	
 public static void main(String[] args) {
-	int[] a= {1,2,3};
+	int[] a= {1,2,3,1,4,2};
 	List<Integer> list=Arrays.stream(a).boxed().distinct().collect(Collectors.toList());
 	
 	System.out.println(list);
