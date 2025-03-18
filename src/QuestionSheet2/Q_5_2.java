cc2877f93d7e2750d51846c439ee7d60acdb089c
twinkle-thakur <twinklethakur7879@gmail.com>
2025-03-18 15:14:18 +0530

    duplicates removal using Set

--- src/QuestionSheet2/Q_5_2.java
+++ src/QuestionSheet2/Q_5_2.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Q_5_2 {
 public static void main(String[] args) {
 	int[] a= {1,2,3,1,4,2};
-	Set<Integer> set=new HashSet<Integer>();
+	Set<Integer> set=new HashSet<Integer>();//we can use linkedhashset also if we want to preserve order
 	for(int i=0;i<a.length;i++) {
 		set.add(a[i]);
 	}
