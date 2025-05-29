61003f0c59608140aba6301426fa3e1bf133c095
twinkle-thakur <twinklethakur7879@gmail.com>
2025-05-29 14:02:39 +0530

    Missing and Repeating number

--- src/QuestionSheet2/Q_9.java
+++ src/QuestionSheet2/Q_9.java
@@ -5,19 +5,22 @@ import java.util.Arrays;
 public class Q_9 {
 public static void missingAndRepeating(int[] arr) {
 	Arrays.sort(arr);
-	int a=0,b=0;
+	int a=1,b=0;
 	for(int i=0;i<arr.length;i++) {
-		if(arr[i]!=1+i) {
-			a=i+1;
+		if(arr[i]==a) {
+			a++;
+		}
+	}
+	for(int i=0;i<arr.length-1;i++) {
+		if(arr[i]==arr[i+1]) {
 			b=arr[i];
 			break;
 		}
-		
 	}
 	System.out.println("missing :"+a+" "+" repeating :"+b);
 }
 public static void main(String[] args) {
-	int[] a= {1, 3, 3};
+	int[] a= {4, 3, 6, 2, 1, 1};
 	missingAndRepeating(a);
 }
 }
